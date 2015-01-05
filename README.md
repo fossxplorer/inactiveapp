6a31cce45f7b7a877773cfe9d03f57a2e54e5392
Bernhard Posselt <dev@bernhard-posselt.com>
2015-01-05 13:49:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Inactive Apps
+Place this app in **owncloud/apps/**
+
+Saves which user accessed an app when
---
+++ appinfo/app.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * ownCloud - inactiveapps
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Bernhard Posselt <dev@bernhard-posselt.com>
+ * @copyright Bernhard Posselt 2015
+ */
+
